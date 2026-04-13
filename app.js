736ee30d8947aec19bf2c24a08fe2c0ae5642cda
RohanS358 <kce080bct033@khwopa.edu.np>
2026-04-13 11:37:37 +0545

    feature: fixing deployment

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = process.env.PORT || 3000
-const app = next({ dev, hostname, port })
+const hostname = process.env.HOST || '0.0.0.0'
+const port = Number(process.env.PORT || 3000)
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
@@ -26,7 +26,7 @@ app.prepare().then(() => {
       res.statusCode = 500
       res.end('internal server error')
     }
-  }).listen(port, (err) => {
+  }).listen(port, hostname, (err) => {
     if (err) throw err
     console.log(`> Ready on http://${hostname}:${port}`)
   })
